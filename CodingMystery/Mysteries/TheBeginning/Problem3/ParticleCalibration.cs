b4730e603a0ddf5d51f31b4a161f3bc5e7ecdcbb
Henrik <henriks@gmail.com>
2022-02-06 01:18:53 +0100

    Problem 3 solved

---
+++ CodingMystery/Mysteries/TheBeginning/Problem3/ParticleCalibration.cs
@@ -0,0 +1,30 @@
+﻿using CodingMystery.Common.CoordinateSystems;
+
+namespace CodingMystery.Mysteries.TheBeginning.Problem3;
+
+internal class ParticleCalibration
+{
+    public void Run()
+    {
+        var input = FileReader.Read("/Mysteries/TheBeginning/Problem3/ParticleGrid.txt");
+        var matrix = MatrixBuilder.BuildCharMatrix(input);
+
+        var center = new MatrixCoord(51, 26);
+        var totalDistance = 0;
+        for (var y = 0; y < matrix.Height; y++)
+        {
+            for (var x = 0; x < matrix.Width; x++)
+            {
+                var coord = new MatrixCoord(x, y);
+                matrix.MoveTo(coord);
+                var v = matrix.ReadValue();
+                if (v == '•')
+                {
+                    totalDistance += coord.ManhattanDistanceTo(center);
+                }
+            }
+        }
+
+        Console.WriteLine(totalDistance);
+    }
+}
--- CodingMystery/Program.cs
+++ CodingMystery/Program.cs
@@ -1,8 +1,12 @@
 ﻿using CodingMystery.Mysteries.TheBeginning.Problem1;
 using CodingMystery.Mysteries.TheBeginning.Problem2;
+using CodingMystery.Mysteries.TheBeginning.Problem3;
 
 //var shreddedPaper = new ShreddedPaper();
 //shreddedPaper.Run();
 
-var tunnelSystem = new TunnelSystem();
-tunnelSystem.Run();
+//var tunnelSystem = new TunnelSystem();
+//tunnelSystem.Run();
+
+var particleCalibration = new ParticleCalibration();
+particleCalibration.Run();
